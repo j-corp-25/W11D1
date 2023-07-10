a04060673d29cbcd160d053131e3a5c7414e5b39
Jordy <jordycorporan95@gmail.com>
2023-07-10 15:31:46 -0400

    Learning forms

--- into_to_react_forms/src/form.js
+++ into_to_react_forms/src/form.js
@@ -6,10 +6,20 @@ function Form () {
         email: "",
         phoneNumber: "",
         phoneType: "home",
-        // staff: "Student",
+        staff: "",
         bio: "",
-        notifications: "checked"
+        notifications: ""
     });
+
+    const handleChange = (field) => {
+        return (e) => {
+            const newObj = Object.assign({},user[field])
+            newObj[field] = e.target.value
+            setUser(newObj)
+      };
+    };
+
+
 
 
 
@@ -17,14 +27,14 @@ function Form () {
         <>
         <h1> Sign Up!</h1>
         <form>
-            <input  type="text" placeholder='Name' value={user.name}></input>
+            <input  type="text" placeholder='Name' value={user.name} onChange={handleChange("name")}></input>
             <br />
-            <input  type="email" placeholder='Email'value={user.email}></input>
+            <input  type="email" placeholder='Email'value={user.email} onChange={handleChange("email")}></input>
             <br />
-            <input  type="text" placeholder='Phone number' value={user.phoneNumber}></input>
+            <input  type="text" placeholder='Phone number' value={user.phoneNumber} onChange={handleChange("phoneNumber")}></input>
             <br />
             <label> Phone Type:
-                <select name="Phonetypes" id="types" value={user.phoneType}>
+                <select name="Phonetypes" id="types" value={user.phoneType} onChange={handleChange("phoneType")}>
                     <option value="home"> Home </option>
                     <option value="work"> Work </option>
                     <option value="mobile"> Mobile</option>
@@ -32,16 +42,16 @@ function Form () {
             </label>
             <br />
             <label> Instructor:
-            <input type="radio" name="staff" value="Instructor"></input>
+            <input type="radio" name="staff" value="Instructor" checked={user.staff === "Instructor"} onChange={handleChange("staff")}></input>
             </label>
             <label> Student:
-            <input type="radio" name="staff" value="Student"></input>
+            <input type="radio" name="staff" value="Student" checked={user.staff === "Student"} onChange={handleChange("staff")}></input>
             </label>
             <br />
-            <textarea rows="5" cols="20" placeholder="Bio" value={user.bio}></textarea>
+            <textarea rows="5" cols="20" placeholder="Bio" value={user.bio} onChange={handleChange("bio")}></textarea>
             <br />
             <label> Sign up for email notifications:
-            <input type="checkbox" name="notifications" value={user.notifications}></input>
+            <input type="checkbox" name={user.notifications} onChange={handleChange("notifications")}></input>
             </label>
             <br />
             <input type="submit" ></input>
